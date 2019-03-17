935d858ed6342eaf152bbc4758f2c45f083e06c1
gowriprathap <gowrinp@gmail.com>
2019-03-17 19:17:07 -0400

    Final commit

---
+++ README.md
@@ -0,0 +1,10 @@
+ARCADE Game
+
+The player can use the up, down, left and right arrow keys to operate.
+The player must reach the water using the arrow keys by avoiding collisions with the enemy bugs.
+Collision with the enemy bugs results in the player being placed at initial position again.
+On 15 collisions, the game is over.
+The "score" gives the number of collisions suffered by the player.
+The "moves" give the number of moves the player has made.
+Reaching the water ends the game and will let the player know how many moves, collisions and time it took to finish the game.
+ENJOY!
--- js2/app2.js
+++ js2/app2.js
@@ -1,45 +1,48 @@
-var t;
-var score = 0;
-var moves = 0;
+var t; //time variable
+var score = 0; //score keeping
+var moves = 0; //number of moves
 moveChanger();
-//scoreDiv.innerHTML = 'Score ' + score;
-//updateDisplay();
-//var numberOfCollisions = 0;
-var moves = 0;
 var seconds = 0; //seconds for timer
 var minutes = 0; //minutes for timer
-var hours = 0;
-var Enemy = function(x, y, speed)
+var hours = 0; //hours for timer
+
+var Enemy = function(x, y, speed) //enemy function
 {
     this.x = x;
     this.y = y;
     this.speed = speed;
-    this.sprite = 'img2/enemy-bug.png';
+    this.sprite = 'img2/enemy-bug.png'; //the enemy image
 };
-Enemy.prototype.update = function(dt){
-  this.x = this.x + this.speed * dt;
-    if (this.x >= 505) {
-        this.x = 0;
-    }
-    this.Collision();
+
+Enemy.prototype.update = function(dt)
+{
+  this.x = this.x + this.speed * dt; //updating the position of the enemy bug
+  if (this.x >= 505) //if the enemy moves offscreen
+  {
+     this.x = 0; //comes back to beginning of screen
+  }
+  this.Collision(); //check if enemy collides with player
 };
+
 Enemy.prototype.render = function()
 {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); //to create the enemy bugs
 };
-Enemy.prototype.Collision = function() {
+
+Enemy.prototype.Collision = function() //collision function
+{
     if (player.y == this.y && player.x <= this.x + 63 && player.x + 65 >= this.x) //taking into account the width of the player
     {
-        console.log('collision');
-        gameOver();
-        //moves++;
-        if (score == 3){
-          gameOverFinal();
+        gameOver(); //changing enemy bug speed
+        if (score == 15) //used up too many moves
+        {
+          gameOverFinal(); //resetting the game, timer, moves and score
         }
-        gameReset();
+        gameReset(); //resetting
     }
 };
-var Player = function(x, y, speed)
+
+var Player = function(x, y, speed) //player function
 {
   this.x = x;
   this.y = y;
@@ -48,118 +51,119 @@ var Player = function(x, y, speed)
 };
 
 Player.prototype.update = function(){};
+
 Player.prototype.render = function()
 {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); //obtaining player image
 };
-Player.prototype.handleInput = function(key){
+
+Player.prototype.handleInput = function(key) //when user uses keys to move the player
+{
   if (key == 'left')
   {
-    if (moves == 0)
+    if (moves == 0) //first move starts the timer
     {
       timer1();
     }
-    this.x -= 101;
-    if (this.x < -50)
+    this.x -= 101; //changing the position
+    if (this.x < -50) //if the player moves offscreen
     {
       this.x = 505;
     }
-    moves++;
-    moveChanger();
-    //console.log(moves);
+    moves++; //incrementing moves
+    moveChanger(); //displaying changed moves
   }
-else if (key == 'right')
+  else if (key == 'right')
   {
-    if (moves == 0)
+    if (moves == 0) //first move starts the timer
     {
       timer1();
     }
-    this.x += 101;
-    if (this.x >= 550)
+    this.x += 101; //changing the position
+    if (this.x >= 550) //if player moves offscreen
     {
       this.x = 0;
     }
-    moves++;
-    moveChanger();
+    moves++; //incrementing moves
+    moveChanger(); //displaying changed moves
   }
-  else if (key == 'up' && this.y > -35){
-    if (moves == 0)
+  else if (key == 'up' && this.y > -35) //do not move up, if it is already at the upper limit
+  {
+    if (moves == 0) //first move starts timer
     {
       timer1();
     }
-    //console.log(380 - (Math.floor(((Math.random()+1) * 3) - 1) * 83));
     if (this.y == 48)
     {
-      console.log("You won");
       wonGame();
     }
+    moves++; //incrementing moves
+    moveChanger();
+    this.y -= 83; //changing position
+  }
+  else if (key == 'down' && this.y < 380) //if key is down
+  {
+    if (moves == 0) //first move
+    {
+      timer1();
+    }
+    this.y += 83; //change position
     moves++;
     moveChanger();
-    this.y -= 83;
     }
-    else if (key == 'down' && this.y < 380)
-    {
-      if (moves == 0)
-      {
-        timer1();
-      }
-      this.y += 83;
-      moves++;
-      moveChanger();
-    }
-}
-Player.prototype.reset = function() {
+  }
+
+Player.prototype.reset = function() //resetting the players position to initial
+{
     this.x = 203;
     this.y = 380;
 };
 
-var allEnemies = [];
-// Place the player object in a variable called player
-var player = new Player(203, 380, 50); //203, 380, 50
-//var enemy = new Enemy(100, 100, 50);
-function moveChanger(){
+var allEnemies = []; //empty enemies array
+var player = new Player(203, 380, 50); // Place the player object in a variable called player
+function moveChanger()
+{
 document.getElementById('moves1').innerHTML = moves + " moves";
 }
-var scoreDiv = document.createElement('div');
+var scoreDiv = document.createElement('div'); //the score div
 gameReset(); // setup defaults
 var canvasDiv = document.getElementsByTagName('canvas')[0];
 document.body.insertBefore(scoreDiv, canvasDiv);
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+// This listens for key presses and sends the keys to your Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e)
+{
+    var allowedKeys =
+    {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
-var score = 0;
-function gameReset() {
+
+function gameReset()
+{
     player.reset();
-    //score = 0;
     updateDisplay();
     allEnemies = [];
-    allEnemies.push(
-        new Enemy(0, 214, Math.random() * 100 + 100),
+    allEnemies.push
+    (
+        new Enemy(0, 214, Math.random() * 100 + 100), //the enemy bugs
         new Enemy(0, 131, Math.random() * 100 + 100),
         new Enemy(0, 48, Math.random() * 100 + 100),
         new Enemy(0, 380 - (Math.floor(((Math.random()+1) * 3) - 1) * 83), Math.random() * 100 + 50),
-        new Enemy(0, 380 - (Math.floor(((Math.random()+1) * 3) - 1) * 83), Math.random() * 100 + 50),
-
+        //after the first three bugs on the three rows, these two bugs are randomly placed on rows 1, 2 or 3
     );
 }
 
-
-function gameOver() {
+function gameOver()
+{
     player.reset();
-    score = score + 1;
-    console.log(score);
+    score = score + 1; //the number of collisions
     updateDisplay();
-    if (score % 2 == 0 && allEnemies.length < 4) {
+    if (score % 2 == 0 && allEnemies.length < 4)
+    {
         allEnemies.push(new Enemy(0, Math.random() * 160 + 50, Math.random() * 90 + 70));
     }
 }
@@ -167,77 +171,77 @@ function gameOver() {
 /*
  * updates the on screen score display
  */
-function updateDisplay() {
-  console.log(score);
-    scoreDiv.innerHTML = 'Score ' + score;
+function updateDisplay() //updating score display after each collision
+{
+  scoreDiv.innerHTML = 'Score ' + score;
 }
 
-        function wonGame() //after all cards match
+function wonGame() //after all cards match
+{
+    var modal = document.getElementById('final_Modal'); //modal is displayed
+    document.getElementById("insert1").innerHTML = "Congratulations. You have finished the game in " + moves + " moves in " + minutes + " minutes and " + seconds + " seconds with " + score + " collisions. Do you want to play again?";
+    var span = document.getElementsByClassName("close")[0]; //closing modal
+    modal.style.display = "block";
+    span.onclick = function() //close modal on clicking close button
+    {
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) //close modal on clicking outside the window
+    {
+        if (event.target == modal)
         {
-          //clearTimeout(t);
-          var modal = document.getElementById('final_Modal'); //modal is displayed
-          document.getElementById("insert1").innerHTML = "Congratulations. You have finished the game in " + moves + " moves in " + minutes + " minutes and " + seconds + " seconds with " + score + " collisions. Do you want to play again?";
-          var span = document.getElementsByClassName("close")[0]; //closing modal
-          modal.style.display = "block";
-          span.onclick = function() //close modal on clicking close button
-          {
-            modal.style.display = "none";
-          }
-          window.onclick = function(event) //close modal on clicking outside the window
-          {
-            if (event.target == modal)
-            {
-              modal.style.display = "none";
-            }
-          }
+           modal.style.display = "none";
         }
-        function refreshPage() //refresh page on clicking refresh button or playing again
-        {
-           location.reload(); //reload page
-        }
-        function gameOverFinal(){
-          console.log('hello')
-          //clearTimeout(t);
-          var modal = document.getElementById('final_Modal'); //modal is displayed
-          document.getElementById("insert1").innerHTML = "Sorry, you used up all your moves. Do you want to play again?";
-          var span = document.getElementsByClassName("close")[0]; //closing modal
-          modal.style.display = "block";
-          span.onclick = function() //close modal on clicking close button
-          {
-            modal.style.display = "none";
-          }
-          window.onclick = function(event) //close modal on clicking outside the window
-          {
-            if (event.target == modal)
-            {
-              modal.style.display = "none";
-            }
-          }
+    }
+}
 
-        }
-        function timer1() //stopwatch function
-        {
-           var h3 = document.getElementsByTagName('h3')[0]; //the timer
-           function add()
-           {
-            seconds++; //adding seconds
-            if (seconds >= 60) //more than 60 seconds should be converted to minute
-             {
-                seconds = 0;
-                minutes++;
-                if (minutes >= 60) //more that 60 minutes should be converted to hours
-                {
-                    minutes = 0;
-                    hours++;
-                }
-            }
-            //incrementing time
-              h3.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-              timer();
-            }
-            function timer()
-            {
-              t = setTimeout(add, 1000);
-            }
-        timer();
-        }
+function refreshPage() //refresh page on clicking refresh button or playing again
+{
+   location.reload(); //reload page
+}
+
+function gameOverFinal()
+{
+    var modal = document.getElementById('final_Modal'); //modal is displayed
+    document.getElementById("insert1").innerHTML = "Sorry, you used up all your moves. Do you want to play again?";
+    var span = document.getElementsByClassName("close")[0]; //closing modal
+    modal.style.display = "block";
+    span.onclick = function() //close modal on clicking close button
+    {
+      modal.style.display = "none";
+    }
+    window.onclick = function(event) //close modal on clicking outside the window
+    {
+      if (event.target == modal)
+      {
+        modal.style.display = "none";
+      }
+    }
+}
+
+function timer1() //stopwatch function
+{
+     var h3 = document.getElementsByTagName('h3')[0]; //the timer
+     function add()
+     {
+      seconds++; //adding seconds
+      if (seconds >= 60) //more than 60 seconds should be converted to minute
+       {
+          seconds = 0;
+          minutes++; //adding minutes
+          if (minutes >= 60) //more that 60 minutes should be converted to hours
+          {
+              minutes = 0;
+              hours++;
+          }
+      }
+//incrementing time
+      h3.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+      timer();
+     }
+     function timer()
+     {
+        t = setTimeout(add, 1000); //1000 milliseconds
+     }
+     timer();
+}
